caef4808fe2abd4e1eed78f0af7c18a6a88022a9
LeeEunHak <x771200@naver.com>
2019-03-15 18:31:35 +0900

    2019.03.15

---
+++ C/1008.c
@@ -0,0 +1,17 @@
+/*
+문제: 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+*/
+#include <stdio.h>
+
+int main()
+{
+	double a, b, div;
+
+	scanf("%lf %lf", &a, &b);
+
+	div = a / b;
+
+	printf("%.9f\n", div);
+
+	return 0;
+}
